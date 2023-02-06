b515034c2bc37a3c16f49eb95df7688dccaeabb4
Huayu Zhang <zhanghuayu1233@gmail.com>
2023-02-06 08:37:13 +0000

    sparse array init

---
+++ src/com/ph1nix/sparsearray/SparseArray.java
@@ -0,0 +1,11 @@
+package com.ph1nix.sparsearray;
+
+public class SparseArray {
+    public static void main(String[] args) {
+        // create an original 2D array of 11 * 11
+        // 0 for no pieces, 1 for black pieces, 2 for blue pieces
+        int chessArr1[][] = new int[11][11];
+        chessArr1[1][2] = 1;
+
+    }
+}
