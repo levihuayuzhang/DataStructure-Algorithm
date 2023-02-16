af450733ea4df2f7c6b0b400140f9551d5ed06d3
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-02-16 00:28:50 +0000

    josephus problem finished

--- Java/DataStructures/src/com/ph1nix/linkedlist/JosephusProblem.java
+++ Java/DataStructures/src/com/ph1nix/linkedlist/JosephusProblem.java
@@ -1,15 +1,23 @@
 package com.ph1nix.linkedlist;
 
+
 /**
+ * Josephus Problem
+ * 1. Using Circle Single Linked List
+ * 2. Each people (Boy) as a node
+ * 3. Not killing, but get out of circle
+ *
  * @author Huayu Zhang
  * @create 2023-02-14-2:22 AM
  */
-public class Joseph {
+public class JosephusProblem {
     public static void main(String[] args) {
 
         CircleSingleLinkedList circleSingleLinkedList = new CircleSingleLinkedList();
         circleSingleLinkedList.addBoy(5); // has 5 node
         circleSingleLinkedList.showBoy();
+
+        circleSingleLinkedList.countBoy(1, 2, 5);
     }
 }
 
@@ -60,6 +68,52 @@ class CircleSingleLinkedList {
             tmpBoy = tmpBoy.getNext();
         }
     }
+
+
+    /**
+     * count the sequence of node get out of circle linked list
+     *
+     * @param startNo no of start node
+     * @param countNum count times (after how many times to get out)
+     * @param nums original quantity of node
+     */
+    public void countBoy (int startNo, int countNum, int nums) {
+        if (first == null || startNo < 1 || startNo > nums) {
+            System.err.println("Incorrect input parameters...");
+            return;
+        }
+        Boy tmp = first;
+        while(true) {
+            if (tmp.getNext() == first) { // tmp is pointing at last node
+                break;
+            }
+            tmp = tmp.getNext();
+        }
+        // before count down, move first and tmp for (startNo - 1) times
+        for (int j = 0; j < startNo - 1; j++) {
+            first = first.getNext();
+            tmp = tmp.getNext();
+        }
+        // start count down, move first and tmp for (countNum - 1) times
+        // this would loop in the circle, until only the last one node remain in the circle
+        while(true) {
+            if (tmp == first) { // only has one node
+                break;
+            }
+            for (int j = 0; j < countNum - 1; j++) {
+                first = first.getNext();
+                tmp = tmp.getNext();
+            }
+            // now first point to the node should get out circle
+            System.out.printf("Node %d out!\n", first.getNo());
+            first = first.getNext();
+            tmp.setNext(first);
+        }
+        System.out.printf("The node remain in circle is %d...\n", first.getNo());
+
+    }
+
+
 }
 
 // boy as a node
