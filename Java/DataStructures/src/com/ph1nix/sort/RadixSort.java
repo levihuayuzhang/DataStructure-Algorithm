dfffeb11d69b6e27dfce43b4e1ad4884f639e415
Huayu Zhang <zhanghuayu.dev@gmail.com>
2023-03-22 17:25:42 +0000

    radix sort

---
+++ Java/DataStructures/src/com/ph1nix/sort/RadixSort.java
@@ -0,0 +1,49 @@
+package com.ph1nix.sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class RadixSort {
+    public static void main(String[] args) {
+        int[] arr = {53, 3, 542, 748, 14, 214};
+        radixSort(arr);
+
+    }
+
+    public static void radixSort(int[] arr) {
+
+        int max = arr[0]; // assume first number is the max number
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int maxLength = (max + "").length();
+
+        // 10 bucket, every bucket is a 1D array
+        int[][] bucket = new int[10][arr.length];
+
+        // for recording actual quantity of data in every bucket
+        int[] bucketElementCounts = new int[10];
+
+        for (int i = 0, n = 1; i < maxLength; i++, n *= 10) {
+            for (int j = 0; j < arr.length; j++) {
+                int digitOfElement = arr[j] / n % 10;
+                bucket[digitOfElement][bucketElementCounts[digitOfElement]] = arr[j];
+                bucketElementCounts[digitOfElement]++;
+            }
+
+            int index = 0;
+            for (int k = 0; k < bucketElementCounts.length; k++) {
+                if (bucketElementCounts[k] != 0) {
+                    for (int l = 0; l < bucketElementCounts[k]; l++) {
+                        arr[index++] = bucket[k][l];
+                    }
+                }
+                bucketElementCounts[k] = 0;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
