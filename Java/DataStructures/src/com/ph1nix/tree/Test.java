9aa9b5dc4b8a480919e8e7f6bf0ca2fc7b888010
Huayu Zhang <zhanghuayu.dev@gmail.com>
2023-03-27 21:45:49 +0100

    binary tree search

---
+++ Java/DataStructures/src/com/ph1nix/tree/Test.java
@@ -0,0 +1,15 @@
+package com.ph1nix.tree;
+
+import java.util.ArrayList;
+
+/**
+ * Test
+ *
+ * @author Huayu Zhang
+ * create time: 2023-03-26 07:16:03
+ */
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Object> arrL = new ArrayList<>();
+    }
+}
